5c18ea1f5f5d544f0462db59e7a327b7032da117
lantaris <edik.ponomarenko@mail.ru>
2020-12-22 17:36:14 +0300

    Temp commit

--- build_android.sh
+++ build_android.sh
@@ -6,5 +6,6 @@ rm -rf build
 mkdir -p build
 
 pushd build
-  cmake -DVS_PLATFORM="android" -DANDROID_PLATFORM=29 -DANDROID_QT=ON ..
+    cmake -DKS_PLATFORM="android" -DANDROID_PLATFORM=29 -DANDROID_QT=ON -DANDROID=ON ..
+    make
 popd  
