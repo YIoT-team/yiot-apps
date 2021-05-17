5c0f35fd87fcfcfdfefac23767bbebe4019787a7
Roman Kutashenko <kutashenko@gmail.com>
2021-05-17 19:25:51 +0300

    Remove test messages

--- controll-app/src/js/devices/pc.js
+++ controll-app/src/js/devices/pc.js
@@ -124,7 +124,7 @@ function enableSSH(pc) {
 
 //-----------------------------------------------------------------------------
 function processingText() {
-    return "Hello YIoT!\nSecond line." // Sorry, I tried to take a value from C/C++, but nothing sensible came out. I need more time.
+    return ""
 }
 
 //-----------------------------------------------------------------------------
