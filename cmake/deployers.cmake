45439eaac24b2fab031a07b52f403ba714b0a50e
Roman Kutashenko <kutashenko@gmail.com>
2021-05-25 18:18:01 +0300

    [macOS] Activate signing

--- cmake/deployers.cmake
+++ cmake/deployers.cmake
@@ -140,18 +140,17 @@ elseif(KS_PLATFORM STREQUAL "macos")
 #    COMMAND ${MAC_CODESIGN} --display --verbose=4 --force --deep --timestamp --options runtime -s "${VS_MACOS_IDENT}" ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.app/Contents/Frameworks/Sparkle.framework/Resources/Autoupdate.app/Contents/MacOS/Autoupdate
 #    COMMAND echo "Signing sparkle [fileop]"                  
 #    COMMAND ${MAC_CODESIGN} --display --verbose=4 --force --deep --timestamp --options runtime -s "${VS_MACOS_IDENT}" ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.app/Contents/Frameworks/Sparkle.framework/Resources/Autoupdate.app/Contents/MacOS/fileop
-#    COMMAND echo "Signing bundle..."       
-#    COMMAND ${MAC_CODESIGN}
-#	--display
-#	--verbose=4
-#	--force
-#	--deep
-#	--timestamp
-#	--options runtime
-#	-s "${VS_MACOS_IDENT}"
-#	"${CMAKE_BINARY_DIR}/${PROJECT_NAME}.app"
-    COMMAND echo "Create DMG..."       	
-    COMMAND ${MAC_APPDMG} 
+    COMMAND echo "Signing bundle..."       
+    COMMAND ${MAC_CODESIGN} 
+      --display 
+      --verbose=4 
+      --force 
+      --deep 
+      --timestamp 
+      --options runtime -s "${VS_MACOS_IDENT}" "${CMAKE_BINARY_DIR}/${PROJECT_NAME}.app"
+  
+  COMMAND echo "Create DMG..."       	
+  COMMAND ${MAC_APPDMG} 
 	"${CMAKE_BINARY_DIR}/dmg.json"
 	"${CMAKE_BINARY_DIR}/${PROJECT_NAME}.dmg"
 	-v
--- scripts/build/macos.sh
+++ scripts/build/macos.sh
@@ -31,7 +31,7 @@ pushd "${SCRIPT_FOLDER}/../../"
     pushd ${BUILD_DIR}
         export CHROMIUM_BUILDTOOLS_PATH=/Users/kutashenko/Work/soft/buildtools
         export QTDIR=/Users/kutashenko/Qt/5.15.0/clang_64/
-        cmake -DKS_PLATFORM="macos" -DBUILD_SERVICE_ENABLE=OFF -DCMAKE_PREFIX_PATH=/Users/kutashenko/Qt/5.14.2/clang_64 -DVS_MACOS_IDENT="4825CBED473D4B9B0138EB0A3E4DE6EAD39E93A7" ..
+        cmake -DKS_PLATFORM="macos" -DBUILD_SERVICE_ENABLE=OFF -DCMAKE_PREFIX_PATH=/Users/kutashenko/Qt/5.14.2/clang_64 -DVS_MACOS_IDENT="7FD87844FA13BF5FCA51E9D32D0829BA632BF1EC" ..
         make -j10 ble-provision-app
         make dmg_release
     popd
