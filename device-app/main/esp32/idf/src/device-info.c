636eda2a7fe63d076acafa527d920c440ca76c27
Roman Kutashenko <kutashenko@gmail.com>
2021-07-05 08:12:29 +0300

    [WIP] ESP32 stub

---
+++ device-app/main/esp32/idf/src/device-info.c
@@ -0,0 +1,42 @@
+//  ────────────────────────────────────────────────────────────
+//                     ╔╗  ╔╗ ╔══╗      ╔════╗
+//                     ║╚╗╔╝║ ╚╣╠╝      ║╔╗╔╗║
+//                     ╚╗╚╝╔╝  ║║  ╔══╗ ╚╝║║╚╝
+//                      ╚╗╔╝   ║║  ║╔╗║   ║║
+//                       ║║   ╔╣╠╗ ║╚╝║   ║║
+//                       ╚╝   ╚══╝ ╚══╝   ╚╝
+//    ╔╗╔═╗                    ╔╗                     ╔╗
+//    ║║║╔╝                   ╔╝╚╗                    ║║
+//    ║╚╝╝  ╔══╗ ╔══╗ ╔══╗  ╔╗╚╗╔╝  ╔══╗ ╔╗ ╔╗╔╗ ╔══╗ ║║  ╔══╗
+//    ║╔╗║  ║║═╣ ║║═╣ ║╔╗║  ╠╣ ║║   ║ ═╣ ╠╣ ║╚╝║ ║╔╗║ ║║  ║║═╣
+//    ║║║╚╗ ║║═╣ ║║═╣ ║╚╝║  ║║ ║╚╗  ╠═ ║ ║║ ║║║║ ║╚╝║ ║╚╗ ║║═╣
+//    ╚╝╚═╝ ╚══╝ ╚══╝ ║╔═╝  ╚╝ ╚═╝  ╚══╝ ╚╝ ╚╩╩╝ ║╔═╝ ╚═╝ ╚══╝
+//                    ║║                         ║║
+//                    ╚╝                         ╚╝
+//
+//    Lead Maintainer: Roman Kutashenko <kutashenko@gmail.com>
+//  ────────────────────────────────────────────────────────────
+
+#include "device-info.h"
+#include <string.h>
+
+//-----------------------------------------------------------------------------
+void
+ks_devinfo_manufacturer(vs_device_manufacture_id_t manufacture_id) {
+    memset(manufacture_id, 0, sizeof(vs_device_manufacture_id_t));
+    strncpy((char *)manufacture_id, "YIoT-dev", sizeof(vs_device_manufacture_id_t) - 1);
+}
+
+//-----------------------------------------------------------------------------
+void
+ks_devinfo_device_type(vs_device_type_t device_type) {
+    memset(device_type, 0, sizeof(vs_device_type_t));
+}
+
+//-----------------------------------------------------------------------------
+void
+ks_devinfo_device_serial(vs_device_serial_t serial) {
+    memset(serial, 0, sizeof(vs_device_serial_t));
+}
+
+//-----------------------------------------------------------------------------
--- device-app/main/esp32/idf/src/main.c
+++ device-app/main/esp32/idf/src/main.c
@@ -111,6 +111,9 @@ static void
 _initializer_exec_task(void *pvParameters) {
 
     int i = 0;
+    // const vs_snap_service_t *snap_cfg_server = NULL;
+    // const vs_snap_service_t *snap_info_server = NULL;
+    // const vs_snap_service_t *snap_prvs_server = NULL;
 
     vs_logger_init(VS_LOGLEV_DEBUG);
 
@@ -121,138 +124,144 @@ _initializer_exec_task(void *pvParameters) {
         VS_LOG_INFO("Loop print logging %d ...", i++);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-        gpio_set_level(GPIO_LED, i % 2);
+//         gpio_set_level(GPIO_LED, i % 2);
 
-        if (i > 1) {
-            continue;
-        }
+//         if (i > 1) {
+//             continue;
+//         }
 
-        vs_status_e ret_code;
-        const vs_snap_service_t *snap_cfg_server;
-        const vs_snap_service_t *snap_info_server;
-        const vs_snap_service_t *snap_prvs_server;
+//         vs_status_e ret_code;
 
-        vs_storage_op_ctx_t slots_storage_impl;
-        vs_storage_op_ctx_t tl_storage_impl;
+// ////////////////////////>>>>>>>>>>>>
 
-        // Device parameters
-        vs_device_manufacture_id_t manufacture_id = "TEST_ESP32";
-        vs_device_type_t device_type = "DVB";
-        vs_device_serial_t serial;
-        vs_secmodule_impl_t *secmodule_impl = NULL;
-        // Device specific parameters
-#if GATEWAY
-        uint32_t device_roles = (uint32_t)VS_SNAP_DEV_GATEWAY | (uint32_t)VS_SNAP_DEV_INITIALIZER;
-#else
-        uint32_t device_roles = (uint32_t)VS_SNAP_DEV_THING | (uint32_t)VS_SNAP_DEV_INITIALIZER;
-#endif
+//         vs_storage_op_ctx_t tl_storage_impl;     // TrustList storage
+//         vs_storage_op_ctx_t slots_storage_impl;  // Emulation of HSM's data slots
+//         vs_storage_op_ctx_t secbox_storage_impl; // SecBox storage
 
+//         // Device parameters
+//         // vs_device_manufacture_id_t manufacture_id;
+//         vs_device_type_t device_type;
+//         // vs_device_serial_t serial;
 
-        vs_netif_t *netif_udp_impl = NULL;
-        vs_netif_t *netif_ble_impl = NULL;
+//         // VS_IOT_MEMSET(&manufacture_id, 0, sizeof(manufacture_id));
+//         VS_IOT_MEMSET(&device_type, 0, sizeof(device_type));
+//         // VS_IOT_MEMSET(&serial, 0, sizeof(serial));
 
-        VS_IOT_MEMSET(serial, 0x03, sizeof(serial));
+//         uint8_t *p = (uint8_t *)&device_type;
+//         p[0] = 1;
 
-        // Initialize NVS
-        VS_LOG_INFO("Initialization NVS flash");
-        INIT_STATUS_CHECK(flash_nvs_init(), "NVS Error");
-
-        //
-        // ---------- Create implementations ----------
-        //
-
-        // Initialization netif
-        VS_LOG_DEBUG("Initialization netif");
-        vs_packets_queue_init(vs_snap_default_processor);
-        netif_udp_impl = vs_hal_netif_udp_bcast();
-        netif_ble_impl = vs_hal_netif_ble("VS-DEMO");
+//         ////////////////////////<<<<<<<<<<<<
 
 
-#if 0
-    // Prepare local storage
-    VS_LOG_DEBUG("Initialization Storage 1");
-    STATUS_CHECK(vs_app_prepare_storage("test"), "Cannot prepare storage");
+//         // Device parameters
+//         vs_device_manufacture_id_t manufacture_id = "TEST_ESP32";
+//         // vs_device_type_t device_type = "DVB";
+//         vs_device_serial_t serial;
+//         vs_secmodule_impl_t *secmodule_impl = NULL;
+        
+//         // Device specific parameters
+//         uint32_t device_roles = (uint32_t)VS_SNAP_DEV_THING | (uint32_t)VS_SNAP_DEV_THING;
 
-    // TrustList storage
-    VS_LOG_DEBUG("Initialization Storage 2");
-    STATUS_CHECK(vs_app_storage_init_impl(&tl_storage_impl, vs_app_trustlist_dir(), VS_TL_STORAGE_MAX_PART_SIZE),
-                 "Cannot create TrustList storage");
 
-    // Slots storage
-    VS_LOG_DEBUG("Initialization Storage 3");
-    STATUS_CHECK(vs_app_storage_init_impl(&slots_storage_impl, vs_app_slots_dir(), VS_SLOTS_STORAGE_MAX_SIZE),
-                 "Cannot create Slots storage");
+//         vs_netif_t *netif_udp_impl = NULL;
+//         vs_netif_t *netif_ble_impl = NULL;
 
-    // Soft Security Module
-    VS_LOG_DEBUG("Initialization Soft Security Module");
-    secmodule_impl = vs_soft_secmodule_impl(&slots_storage_impl);
-    // TrustList storage
-    // Slots storage
+//         VS_IOT_MEMSET(serial, 0x03, sizeof(serial));
 
-    //
-    // ---------- Initialize Virgil SDK modules ----------
-    //
+//         // Initialize NVS
+//         VS_LOG_INFO("Initialization NVS flash");
+//         INIT_STATUS_CHECK(flash_nvs_init(), "NVS Error");
 
-    // Provision module
-    VS_LOG_DEBUG("Initialization provision module");
-    ret_code = vs_provision_init(&tl_storage_impl, secmodule_impl);
-    if (VS_CODE_OK != ret_code && VS_CODE_ERR_NOINIT != ret_code) {
-        VS_LOG_ERROR("Cannot initialize Provision module");
-        goto terminate;
+//         //
+//         // ---------- Create implementations ----------
+//         //
+
+//         // Initialization netif
+//         VS_LOG_DEBUG("Initialization netif");
+//         vs_packets_queue_init(vs_snap_default_processor);
+//         netif_udp_impl = vs_hal_netif_udp_bcast();
+//         netif_ble_impl = vs_hal_netif_ble("VS-DEMO");
+
+
+//     // Prepare local storage
+//     VS_LOG_DEBUG("Initialization Storage 1");
+//     STATUS_CHECK(vs_app_prepare_storage("test"), "Cannot prepare storage");
+
+//     // TrustList storage
+//     VS_LOG_DEBUG("Initialization Storage 2");
+//     STATUS_CHECK(vs_app_storage_init_impl(&tl_storage_impl, vs_app_trustlist_dir(), VS_TL_STORAGE_MAX_PART_SIZE),
+//                  "Cannot create TrustList storage");
+
+//     // Slots storage
+//     VS_LOG_DEBUG("Initialization Storage 3");
+//     STATUS_CHECK(vs_app_storage_init_impl(&slots_storage_impl, vs_app_slots_dir(), VS_SLOTS_STORAGE_MAX_SIZE),
+//                  "Cannot create Slots storage");
+
+//     // Soft Security Module
+//     VS_LOG_DEBUG("Initialization Soft Security Module");
+//     secmodule_impl = vs_soft_secmodule_impl(&slots_storage_impl);
+//     // TrustList storage
+//     // Slots storage
+
+//     //
+//     // ---------- Initialize Virgil SDK modules ----------
+//     //
+
+//     // Provision module
+//     VS_LOG_DEBUG("Initialization provision module");
+//     ret_code = vs_provision_init(&tl_storage_impl, secmodule_impl);
+//     if (VS_CODE_OK != ret_code && VS_CODE_ERR_NOINIT != ret_code) {
+//         VS_LOG_ERROR("Cannot initialize Provision module");
+//         goto terminate;
+//     }
+
+//         VS_LOG_DEBUG("Initialization snap");
+//         STATUS_CHECK(
+
+//                 vs_snap_init(netif_udp_impl,
+//                              vs_packets_queue_add,
+//                              need_enc_cb,
+//                              name_change_cb,
+//                              manufacture_id,
+//                              device_type,
+//                              serial,
+//                              device_roles),
+//                 "Unable to initialize SNAP module");
+
+//         VS_LOG_DEBUG("Add BLE network interface");
+//         STATUS_CHECK(vs_snap_netif_add(netif_ble_impl), "Unable to add BLE network interface");
+
+
+//         //
+//         // ---------- Register SNAP services ----------
+//         //
+
+//         //  INFO server service
+//         VS_LOG_DEBUG("Initialization snap info server");
+//         snap_info_server = vs_snap_info_server(NULL);
+//         VS_LOG_DEBUG("Register snap info server");
+//         STATUS_CHECK(vs_snap_register_service(snap_info_server), "Cannot register INFO server service");
+
+//         //  CFG server service
+//         VS_LOG_DEBUG("Initialization snap cfg server");
+//         snap_cfg_server = vs_snap_cfg_server(cfg_server_cb);
+//         VS_LOG_DEBUG("Register snap cfg server");
+//         STATUS_CHECK(vs_snap_register_service(snap_cfg_server), "Cannot register _CFG server service");
+
+//         // Try to start WiFi
+//         start_wifi();
     }
 
-#endif
-
-        VS_LOG_DEBUG("Initialization snap");
-        STATUS_CHECK(
-
-                vs_snap_init(netif_udp_impl,
-                             vs_packets_queue_add,
-                             need_enc_cb,
-                             name_change_cb,
-                             manufacture_id,
-                             device_type,
-                             serial,
-                             device_roles),
-                "Unable to initialize SNAP module");
-
-        VS_LOG_DEBUG("Add BLE network interface");
-        STATUS_CHECK(vs_snap_netif_add(netif_ble_impl), "Unable to add BLE network interface");
-
-
-        //
-        // ---------- Register SNAP services ----------
-        //
-
-        //  INFO server service
-        VS_LOG_DEBUG("Initialization snap info server");
-        snap_info_server = vs_snap_info_server(NULL);
-        VS_LOG_DEBUG("Register snap info server");
-        STATUS_CHECK(vs_snap_register_service(snap_info_server), "Cannot register INFO server service");
-
-        //  CFG server service
-        VS_LOG_DEBUG("Initialization snap cfg server");
-        snap_cfg_server = vs_snap_cfg_server(cfg_server_cb);
-        VS_LOG_DEBUG("Register snap cfg server");
-        STATUS_CHECK(vs_snap_register_service(snap_cfg_server), "Cannot register _CFG server service");
-
-        // Try to start WiFi
-        start_wifi();
-    }
-
-terminate:;
-
-#if 0
     //  PRVS service
-    VS_LOG_DEBUG("Initialization snap prvs service");
-    snap_prvs_server = vs_snap_prvs_server(secmodule_impl);
-    VS_LOG_DEBUG("Register snap prvs service");
-    STATUS_CHECK(vs_snap_register_service(snap_prvs_server), "Cannot register PRVS service");
+    // VS_LOG_DEBUG("Initialization snap prvs service");
+    // snap_prvs_server = vs_snap_prvs_server(secmodule_impl);
+    // VS_LOG_DEBUG("Register snap prvs service");
+    // STATUS_CHECK(vs_snap_register_service(snap_prvs_server), "Cannot register PRVS service");
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 
     while (1);
-terminate:
+// terminate:
     VS_LOG_INFO("Application start error");
      // Deinit Virgil SDK modules
     vs_snap_deinit();
@@ -264,7 +273,6 @@ terminate:
     vs_soft_secmodule_deinit();
 
     while (1);
-#endif
 }
 
 //-----------------------------------------------------------------------------
