5c1b398a45f5d28d0dcf537c552d7a8523aba8f8
Roman Kutashenko <kutashenko@gmail.com>
2021-07-06 08:58:29 +0300

    iOS app works

--- controll-app/src/os/ios/KSiOSWiFi.mm
+++ controll-app/src/os/ios/KSiOSWiFi.mm
@@ -15,33 +15,16 @@
 //                    ╚╝                         ╚╝
 //
 //    Lead Maintainer: Roman Kutashenko <kutashenko@gmail.com>
-//  ────────────────────────────────────────────────────────────
+//  ────────────────────────────────────────────────────────────>
 
-#ifndef PROVISION_QT_WIFI_MACOS_H
-#define PROVISION_QT_WIFI_MACOS_H
+#include <KSWiFi.h>
 
-#include <QtCore>
-
-struct KSWiFiInfo {
-    int rssi;
-    QDateTime lastUpdate;
-
-    KSWiFiInfo(int _rssi) {
-        rssi = _rssi;
-        lastUpdate = QDateTime::currentDateTime();
-    }
-
-    KSWiFiInfo() {
-        rssi = -100;
-        lastUpdate = QDateTime::currentDateTime().addYears(-1);
-    }
-};
-
-typedef QMap<QString, KSWiFiInfo> KSQWiFiNetworks;
-
-#if defined(Q_OS_MACOS) || defined(Q_OS_IOS) || defined(Q_OS_LINUX) || defined(Q_OS_WIN32)
+//-----------------------------------------------------------------------------
 KSQWiFiNetworks
-wifi_enum();
-#endif
+wifi_enum() {
+    KSQWiFiNetworks res;
+    res["test"] = KSWiFiInfo(10);
+    return res;
+}
 
-#endif // PROVISION_QT_WIFI_MACOS_H
+//-----------------------------------------------------------------------------
---
+++ controll-app/src/yiot_qml_plugin_import.cpp
@@ -0,0 +1,22 @@
+// File Generated via CMake script during build time.
+// The purpose of this file is to force the static load of qml plugin during static build
+// Please rerun CMake to update this file.
+// File will be overwrite at each CMake run.
+
+#include <QtPlugin>
+
+Q_IMPORT_PLUGIN(QtQuick2Plugin);
+Q_IMPORT_PLUGIN(QtQuickControls2Plugin);
+Q_IMPORT_PLUGIN(QtQuick2WindowPlugin);
+Q_IMPORT_PLUGIN(QtQuickLayoutsPlugin);
+Q_IMPORT_PLUGIN(QtQmlPlugin);
+Q_IMPORT_PLUGIN(QtQuickTemplates2Plugin);
+Q_IMPORT_PLUGIN(QtQmlModelsPlugin);
+Q_IMPORT_PLUGIN(QtQmlWorkerScriptPlugin);
+Q_IMPORT_PLUGIN(QmlSettingsPlugin);
+Q_IMPORT_PLUGIN(QtQuickControls2FusionStylePlugin);
+Q_IMPORT_PLUGIN(QtQuickControls2UniversalStylePlugin);
+Q_IMPORT_PLUGIN(QtQuickControls2MaterialStylePlugin);
+Q_IMPORT_PLUGIN(QtQuickControls2ImagineStylePlugin);
+Q_IMPORT_PLUGIN(QtGraphicalEffectsPlugin);
+Q_IMPORT_PLUGIN(QtGraphicalEffectsPrivatePlugin);
