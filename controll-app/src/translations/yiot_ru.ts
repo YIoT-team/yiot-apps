ecb7e2f26a3c6605307796218d3fa25e30881013
Roman Kutashenko <kutashenko@gmail.com>
2021-09-27 22:56:16 +0300

    [WIP] Add translations

---
+++ controll-app/src/translations/yiot_ru.ts
@@ -0,0 +1,16 @@
+<!DOCTYPE TS><TS version="2.1" language="ru">
+<context>
+        <name>LeftSideMenu</name>
+        <message>
+                <source>About</source>
+                <translation>О приложении</translation>
+        </message>
+        <message>
+                <source>Settings</source>
+                <translation>Настройки</translation>
+        </message>
+</context>
+<context>
+        <name>Test</name>
+</context>
+</TS>
