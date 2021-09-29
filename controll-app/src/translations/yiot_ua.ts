abe8acb6d3b967ec640fab6cdbb8f5c5d0779323
Roman Kutashenko <kutashenko@gmail.com>
2021-09-29 22:19:41 +0300

    Update Ukrainian translation

--- controll-app/src/translations/yiot_ua.ts
+++ controll-app/src/translations/yiot_ua.ts
@@ -18,6 +18,19 @@
 
 
 <context>
+        <name>Main</name>
+        <message>
+                <source>Do you want to setup a new device ?</source>
+                <translation>Хочете налаштувати новий пристрій ?</translation>
+        </message>
+        <message>
+                <source>Do you want to start work with a new device ?</source>
+                <translation>Хочете почати роботу з новим пристроєм ?</translation>
+        </message>
+</context>
+
+
+<context>
         <name>LeftSideMenu</name>
         <message>
                 <source>Controlled devices</source>
@@ -54,7 +67,7 @@
         </message>
         <message>
                 <source>IoT Networks</source>
-                <translation>IoT Networks</translation>
+                <translation>IoT Мережі</translation>
         </message>
         <message>
                 <source>Event settings</source>
@@ -150,10 +163,301 @@
         </message>
         <message>
                 <source>Default EC: </source>
-                <translation>EC за замовчуванням:</translation>
+                <translation>EC за замовчуванням: </translation>
         </message>
         <!-- TODO: Recovery, Auth, Tl -->
 </context>
+
+
+<context>
+        <name>NetworksSettingsPage</name>
+        <message>
+                <source>IoT Networks</source>
+                <translation>IoT Мережі</translation>
+        </message>
+        <message>
+                <source>Add new Root of trust</source>
+                <translation>Додати новий корінь довіри</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>ThemeListPage</name>
+        <message>
+                <source>Select theme</source>
+                <translation>Виберіть тему</translation>
+        </message>
+        <message>
+                <source>Dark</source>
+                <translation>Темна</translation>
+        </message>
+        <message>
+                <source>Light</source>
+                <translation>Світла</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>ExtDevicesListPage</name>
+        <message>
+                <source>Device types and plugins</source>
+                <translation>Типи пристроїв та плагіни</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>ExtDevicePage</name>
+        <message>
+                <source>YIoT device</source>
+                <translation>YIoT пристрій</translation>
+        </message>
+        <message>
+                <source>Plugins:</source>
+                <translation>Плагіни:</translation>
+        </message>
+        <message>
+                <source>Install</source>
+                <translation>Встановити</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>ExtPluginPage</name>
+        <message>
+                <source>YIoT plugin</source>
+                <translation>YIoT плагін</translation>
+        </message>
+        <message>
+                <source>Install</source>
+                <translation>Встановити</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>ExtIntegrationsListPage</name>
+        <message>
+                <source>Integrations</source>
+                <translation>Інтеграції</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>RoTControlPage</name>
+        <message>
+                <source>Root of trust</source>
+                <translation>Корінь довіри</translation>
+        </message>
+        <message>
+                <source>Name: </source>
+                <translation>Ім’‎я: </translation>
+        </message>
+        <message>
+                <source>ID: </source>
+                <translation>ID: </translation>
+        </message>
+        <message>
+                <source>Default EC: </source>
+                <translation>EC за замовчуванням: </translation>
+        </message>
+        <message>
+                <source>TrustList version: </source>
+                <translation>Версія TrustList: </translation>
+        </message>
+        <message>
+                <source>TrustList keys count: </source>
+                <translation>Кількість TrustList ключів: </translation>
+        </message>
+        <message>
+                <source>Export</source>
+                <translation>Експортувати</translation>
+        </message>
+        <message>
+                <source>Import</source>
+                <translation>Імпортувати</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>RoTExportPage</name>
+        <message>
+                <source>Export Root of trust</source>
+                <translation>Експортувати корінь довіри</translation>
+        </message>
+        <message>
+                <source>Password Hint</source>
+                <translation>Підказка пароля</translation>
+        </message>
+        <message>
+                <source>Export</source>
+                <translation>Експортувати</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>RoTImportPage</name>
+        <message>
+                <source>Import Root of trust</source>
+                <translation>Імпортувати корінь довіри</translation>
+        </message>
+        <message>
+                <source>Password Hint</source>
+                <translation>Підказка пароля</translation>
+        </message>
+        <message>
+                <source>Import</source>
+                <translation>Імпортувати</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>SetupProcessingPage</name>
+        <message>
+                <source>Device initialisation</source>
+                <translation>Ініціалізація пристрою</translation>
+        </message>
+        <message>
+                <source>Select WiFi network</source>
+                <translation>Виберіть мережу WiFi</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>CmdProcessingPage</name>
+        <message>
+                <source>Device initialisation</source>
+                <translation>Ініціалізація пристрою</translation>
+        </message>
+        <message>
+                <source>Connecting</source>
+                <translation>З’єднання</translation>
+        </message>
+        <message>
+                <source>Data send</source>
+                <translation>Надсилання даних</translation>
+        </message>
+        <message>
+                <source>Processing ...</source>
+                <translation>Обробка ...</translation>
+        </message>
+        <message>
+                <source>Disconnecting</source>
+                <translation>Відключення</translation>
+        </message>
+        <message>
+                <source>Stop</source>
+                <translation>Стоп</translation>
+        </message>
+        <message>
+                <source>DONE</source>
+                <translation>ЗРОБЛЕНО</translation>
+        </message>
+        <message>
+                <source>Error occured :(</source>
+                <translation>Сталася помилка :(</translation>
+        </message>
+        <message>
+                <source>Ok</source>
+                <translation>Ок</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>DevicesPage</name>
+        <message>
+                <source>Controlled devices</source>
+                <translation>Контрольовані пристрої</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>DeviceInfoPopup</name>
+        <message>
+                <source>Name :</source>
+                <translation>Ім’‎я :</translation>
+        </message>
+        <message>
+                <source>Addr :</source>
+                <translation>Адреса :</translation>
+        </message>
+        <message>
+                <source>ID :</source>
+                <translation>ID :</translation>
+        </message>
+        <message>
+                <source>Manufacturer :</source>
+                <translation>Виробник :</translation>
+        </message>
+        <message>
+                <source>Type :</source>
+                <translation>Тип :</translation>
+        </message>
+        <message>
+                <source>Roles :</source>
+                <translation>Ролі :</translation>
+        </message>
+        <message>
+                <source>Firmware version :</source>
+                <translation>Версія прошивки :</translation>
+        </message>
+        <message>
+                <source>TrustList version :</source>
+                <translation>Версія TrustList :</translation>
+        </message>
+        <message>
+                <source>Sent packets :</source>
+                <translation>Надіслано пакетів :</translation>
+        </message>
+        <message>
+                <source>Received packets :</source>
+                <translation>Отримано пакетів :</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>DevicesSetupPage</name>
+        <message>
+                <source>Devices setup</source>
+                <translation>Налаштування пристроїв</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>SharePage</name>
+        <message>
+                <source>Share control</source>
+                <translation>Поділіться контролем</translation>
+        </message>
+</context>
+
+
+<!-- EXTENSIONS -->
+<context>
+        <name>Control</name>
+        <message>
+                <source>Enable</source>
+                <translation>Ввімкнути</translation>
+        </message>
+        <message>
+                <source>Disable</source>
+                <translation>Вимкнути</translation>
+        </message>
+</context>
+<!-- EXTENSIONS -->
+
 
 <context>
         <name>AboutPage</name>
@@ -162,6 +466,8 @@
                 <translation>Про YIoT</translation>
         </message>
 </context>
+
+
 <context>
         <name>FormVendor</name>
         <message>
@@ -169,6 +475,8 @@
                 <translation>Про YIoT</translation>
         </message>
 </context>
+
+
 <context>
         <name>FormDoers</name>
         <message>
@@ -178,6 +486,28 @@
         <message>
                 <source>Sponsors</source>
                 <translation>Спонсори</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>Sponsors</name>
+        <message>
+                <source>> Support us :)</source>
+                <translation>> Підтримати нас :)</translation>
+        </message>
+        <message>
+                <source>No, per now :)</source>
+                <translation>Немає, поки що :)</translation>
+        </message>
+</context>
+
+
+<context>
+        <name>Contributors</name>
+        <message>
+                <source>> Contribute with us :)</source>
+                <translation>> Співпрацювати з нами :)</translation>
         </message>
 </context>
 
