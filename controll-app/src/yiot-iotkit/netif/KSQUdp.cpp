37ede7c7203396b2b9f93834e74535354f17afba
Andr <andr@yiot.dev>
2023-11-21 14:40:16 +0200

    UDP transport improvements

--- controll-app/src/yiot-iotkit/netif/KSQUdp.cpp
+++ controll-app/src/yiot-iotkit/netif/KSQUdp.cpp
@@ -50,10 +50,11 @@ KSQUdp::KSQUdp(QHostAddress subnet, quint16 port) : m_subnet(subnet), m_port(por
 //-----------------------------------------------------------------------------
 bool
 KSQUdp::init() {
-
     if (!m_socket.bind(m_port, QUdpSocket::ReuseAddressHint)) {
         VS_LOG_ERROR("Unable to bind LocalHost:%d. Last error : %s", m_port, VSQCString(m_socket.errorString()));
+#if 0
         return false;
+#endif
     }
 
     for (auto &interface : QNetworkInterface::allInterfaces()) {
@@ -97,8 +98,21 @@ KSQUdp::_internal_tx(const uint8_t *data, const uint16_t data_sz) {
         vs_snap_packet_dump("OUT", packet);
     }
 
-    auto sentBytes = m_socket.writeDatagram(
-            QByteArray::fromRawData(reinterpret_cast<const char *>(data), data_sz), m_subnet, m_port);
+    uint16_t sentBytes = 0;
+
+    if (!m_socket.isValid()) {
+        deinit();
+        init();
+    }
+
+    try {
+        if (m_socket.isValid()) {
+            sentBytes = m_socket.writeDatagram(
+                    QByteArray::fromRawData(reinterpret_cast<const char *>(data), data_sz), m_subnet, m_port);
+        }
+    } catch (...) {
+    }
+
 
     if (sentBytes != data_sz) {
         VS_LOG_ERROR("Sent bytes : %d, data bytes to send : %d. Last error : %s",
